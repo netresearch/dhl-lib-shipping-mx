a6e07ba564a6f1a0b92bdad63b8927b9be665a78
Christoph Aßmann <christoph.assmann@netresearch.de>
2017-02-01 18:14:15 +0100

    DHLVM2-24: prepare webservice access

---
+++ Api/Webservice/Adapter/GkAdapterInterface.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Dhl Versenden
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to
+ * newer versions in the future.
+ *
+ * PHP version 7
+ *
+ * @category  Dhl
+ * @package   Dhl\Versenden\Api
+ * @author    Christoph Aßmann <christoph.assmann@netresearch.de>
+ * @copyright 2017 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+namespace Dhl\Versenden\Api\Webservice\Adapter;
+
+use \Dhl\Versenden\Api\Webservice\AdapterInterface;
+use \Dhl\Versenden\Api\Webservice\Request\Type\GetVersionRequestInterface;
+use \Dhl\Versenden\Api\Webservice\Response\Parser\GetVersionParserInterface;
+use \Dhl\Versenden\Api\Webservice\Request\Type\DeleteShipmentRequestInterface;
+use \Dhl\Versenden\Api\Webservice\Response\Parser\DeleteShipmentParserInterface;
+
+/**
+ * Geschäftskunden API Adapter
+ *
+ * @category Dhl
+ * @package  Dhl\Versenden\Api
+ * @author   Christoph Aßmann <christoph.assmann@netresearch.de>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     http://www.netresearch.de/
+ */
+interface GkAdapterInterface extends AdapterInterface
+{
+    /**
+     * @param \Dhl\Versenden\Api\Webservice\Request\Type\GetVersionRequestInterface $request
+     * @param \Dhl\Versenden\Api\Webservice\Response\Parser\GetVersionParserInterface $parser
+     *
+     * @return \Dhl\Versenden\Api\Webservice\Response\Type\GetVersionResponseInterface
+     */
+    public function getVersion(GetVersionRequestInterface $request, GetVersionParserInterface $parser);
+
+    /**
+     * @param \Dhl\Versenden\Api\Webservice\Request\Type\DeleteShipmentRequestInterface $request
+     * @param \Dhl\Versenden\Api\Webservice\Response\Parser\DeleteShipmentParserInterface $parser
+     *
+     * @return \Dhl\Versenden\Api\Webservice\Response\Type\DeleteShipmentResponseInterface
+     */
+    public function deleteShipmentOrder(DeleteShipmentRequestInterface $request, DeleteShipmentParserInterface $parser);
+}
