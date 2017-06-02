c07b51f7ceaab9523a853333365089ebf7330441
Benjamin Heuer <benjamin.heuer@netresearch.de>
2017-06-02 14:29:02 +0200

    DHLVM2-78: provide Exception handling for soft and hard validation errors

---
+++ Gla/Response/Type/ShipmentErrorResponseReasonsType.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Dhl Shipping
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to
+ * newer versions in the future.
+ *
+ * PHP version 7
+ *
+ * @category  Dhl
+ * @package   Dhl\Shipping\Webservice
+ * @author    Benjamin Heuer <benjamin.heuer@netresearch.de>
+ * @copyright 2017 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+
+namespace Dhl\Shipping\Gla\Response\Type;
+
+/**
+ * ShipmentErrorResponseReasonType
+ *
+ * @category Dhl
+ * @package  Dhl\Shipping\Webservice
+ * @author   Benjamin Heuer <benjamin.heuer@netresearch.de>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     http://www.netresearch.de/
+ */
+class ShipmentErrorResponseReasonsType
+{
+    /**
+     * @var string[]
+     */
+    private $msg;
+
+    /**
+     * @return string[]
+     */
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    /**
+     * @param string[] $msg
+     */
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+    }
+}
