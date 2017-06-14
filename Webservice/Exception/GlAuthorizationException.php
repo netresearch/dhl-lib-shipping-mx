0841bffee276e8f6758411c11c9719e90d30f1ba
Benjamin Heuer <benjamin.heuer@netresearch.de>
2017-06-14 15:31:26 +0200

    DHLVM2-90: add logging to webservice

--- Webservice/Exception/GlAuthorizationException.php
+++ Webservice/Exception/GlAuthorizationException.php
@@ -35,7 +35,7 @@ namespace Dhl\Shipping\Webservice\Exception;
  * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link     http://www.netresearch.de/
  */
-class GlAuthorizationException extends \Exception
+class GlAuthorizationException extends GlOperationException
 {
     /**
      * @param string $responseBody
